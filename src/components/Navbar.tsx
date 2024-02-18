dfcc5792f351c85f48522dcdb8a0968df644f105
JRafaCode <rafaelromero.mp@gmail.com>
2024-02-18 12:05:41 -0300

    Login Finish

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link";
-import {signIn, useSession} from "next-auth/react"
+import {signIn, useSession, signOut} from "next-auth/react"
 
 function Navbar() {
 
@@ -14,14 +14,24 @@ function Navbar() {
                 <h1>NextGoogle</h1>
             </Link>
 
-            <div className="flex gap-x-2 item-center">
+            {session ?.user ?(
+                <div className="flex gap-x-2 item-center">
                 <Link href="/dashboard">
                     Dashboard
                 </Link>    
+                <p>{session.user.name} {session.user.email}</p>
+                <img src={session.user.image} alt=""
+                className="w-10 h-10 rounded-full cursor-pointer"/>
+                <button onClick={() => { signOut() }}>
+                    Logout
+                </button>
+                      
+            </div>
+            ): (
                 <button onClick={() => signIn()} className="bg-sky-400 px-3 py-2 rounded">
                     SignIn 
-                </button>         
-            </div>
+                </button>   
+            )}
         </nav>
     );
 }
